9186a5cf6ebf0ed4788dae851815d1e821942aef
eggerino <eggensberger.armin@googlemail.com>
2026-01-06 13:29:09 +0100

    added cmake

---
+++ test/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "tests.h"
+
+int main(void) {
+    test_interpolation();
+    printf("success\n");
+    return 0;
+}
--- test/test_interpolation.c
+++ test/test_interpolation.c
@@ -1,17 +1,12 @@
 #include <assert.h>
-#include <stdio.h>
 
+#include "tests.h"
 #include "ts.h"
 
-void test_interpolate_linear1() {
+void test_interpolation(void) {
     assert(0 == ts_interpolate_linear1(0, 1, 0));
     assert(1 == ts_interpolate_linear1(0, 1, 1));
     assert(2 == ts_interpolate_linear1(0, 1, 2));
     assert(-1 == ts_interpolate_linear1(0, 1, -1));
     assert(1 == ts_interpolate_linear1(1, 1, -1));
-}
-
-int main() {
-    test_interpolate_linear1();
-    printf("Success\n");
 }
