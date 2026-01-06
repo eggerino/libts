5f9169e055b7bb4e6f780e45e123bc1c4037b1af
eggerino <eggensberger.armin@googlemail.com>
2026-01-06 16:17:49 +0100

    make test for each module

--- test/test_interpolation.c
+++ test/test_interpolation.c
@@ -1,12 +1,13 @@
 #include <assert.h>
 
-#include "tests.h"
 #include "ts.h"
 
-void test_interpolation(void) {
+void test_interpolate_linear1(void) {
     assert(0 == ts_interpolate_linear1(0, 1, 0));
     assert(1 == ts_interpolate_linear1(0, 1, 1));
     assert(2 == ts_interpolate_linear1(0, 1, 2));
     assert(-1 == ts_interpolate_linear1(0, 1, -1));
     assert(1 == ts_interpolate_linear1(1, 1, -1));
 }
+
+int main(void) { test_interpolate_linear1(); }
--- test/tests.h
+++
@@ -1,6 +0,0 @@
-#ifndef TESTS_H_
-#define TESTS_H_
-
-void test_interpolation(void);
-
-#endif  // TESTS_H_
