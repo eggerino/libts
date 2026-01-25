cbf4c1ee3ea8bcbc817e865c489816226cf20c03
eggerino <eggensberger.armin@googlemail.com>
2026-01-25 11:43:39 +0100

    added tests for linear3

--- tests/interpolation.c
+++ tests/interpolation.c
@@ -1,37 +1,44 @@
 #include "common.h"
 #include "ts.h"
 
-// void test_interpolate_linear1(void) {
-//     assert_eps(0, ts_interpolate_linear1(0, 1, 0));
-//     assert_eps(1, ts_interpolate_linear1(0, 1, 1));
-//     assert_eps(2, ts_interpolate_linear1(0, 1, 2));
-//     assert_eps(-1, ts_interpolate_linear1(0, 1, -1));
-//     assert_eps(1, ts_interpolate_linear1(1, 1, -1));
-// }
+void test_linear3(void) {
+    ts_linear3_state s = {0};
+    ts_vec3 start = {1, 2, 3};
+    ts_vec3 end = {4, 6, 8};
+    ts_f64 x = 0.25;
+    ts_vec3 act = {0};
+    ts_vec3 exp = {1.75, 3, 4.25};
 
-// void test_interpolate_linear3(void) {
-//     ts_vec3 expected = {1.75, 2, 2.5};
-//     ts_vec3 actual = {0};
-//     ts_f64 x = 0.25;
+    ts_linear3_init(&start, &end, &s);
+    ts_linear3(&s, x, &act);
+    assert_vec3(exp, act);
+}
 
-//     ts_interpolate_linear3(&((ts_vec3){1, 2, 3}), &((ts_vec3){4, 2, 1}), x, &actual);
+void test_linear3_d(void) {
+    ts_linear3_state s = {0};
+    ts_vec3 start = {1, 2, 3};
+    ts_vec3 end = {4, 6, 8};
+    ts_vec3 act = {0};
+    ts_vec3 exp = {3, 4, 5};
 
-//     assert_vec3(expected, actual);
-// }
+    ts_linear3_init(&start, &end, &s);
+    ts_linear3_d(&s, &act);
+    assert_vec3(exp, act);
+}
 
-// void test_interpolate_slerp(void) {
-//     ts_quat expected = {0.8660254037844386, 0.28867513459481287, 0.28867513459481287, 0.28867513459481287};
-//     ts_quat actual = {0};
-//     ts_f64 x = 0.5;
+void test_linear3_dd(void) {
+    ts_vec3 act = {0};
+    ts_vec3 exp = {0, 0, 0};
 
-//     ts_interpolate_slerp(&((ts_quat){1, 0, 0, 0}), &((ts_quat){.5, .5, .5, .5}), x, &actual);
+    ts_linear3_dd(&act);
+    assert_vec3(exp, act);
+}
 
-//     assert_quat(expected, actual);
-// }
+// TODO slerp, bezier, poly5
 
 int main(void) {
-    // test_interpolate_linear1();
-    // test_interpolate_linear3();
-    // test_interpolate_slerp();
+    test_linear3();
+    test_linear3_d();
+    test_linear3_dd();
     return 0;
 }
